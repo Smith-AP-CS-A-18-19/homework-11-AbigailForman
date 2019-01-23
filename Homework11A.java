1b4902a7df9bc30ff5fa9a918ac4131bf3eb8fb5
Abby Forman <formaabb@berkeleyprep.org>
2019-01-22 19:50:27 -0500

    Abby Forman

---
+++ Homework11A.java
@@ -0,0 +1,19 @@
+import java.util.ArrayList;
+
+public void Homework11A(int c) {
+	c = constructor;
+	for (int i : blist) {
+		if (n % i == 0) {
+		}
+	}
+
+
+  public int problem4() {
+  	ArrayList<Integer> list = new ArrayList<Integer>();
+  	for(int i = 0; i < num; i++){
+  		int x = (int)((Math.random()) * (maxVal+1));
+  		list.add(x);
+  	}
+  	return list;
+
+  }
